cf26467ca2f2ef8d7c05074e065ff92b7c1d0522
Trey <tbeau5595@hotmail.com>
2019-01-02 18:22:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ function takeANumber(katzDeli, name) {
 }
 
 function nowServing(katzDeli) {
-  let i = 0;
-  while(i < katzDeli.length) {
-    i++
-  }
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
+  else {
+    let current = katzDeli.shift();
+    return (`Currently serving ${current}.`);
+  }
 }
 
-function currentLine(line){
-  if(!line.length) {
+function currentLine(line) {
+  if (!line.length) {
     return "The line is currently empty.";
   }
-  var lineNamesandNumbers = [];
   
-  for(var i=0; i<line.length; i++) {
-    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  let lineNamesandNumbers = [];
+  
+  for (let i = 0; i < line.length; i++) {
+    lineNamesandNumbers.push(`${i + 1}: ${line[i]}`);
   }
-  console.log("The line is currently: " + lineNamesandNumbers)
-  return "The line is currently: " + lineNamesandNumbers.join(', ');
+  
+  return (`The line is currently: ${lineNamesandNumbers.join(', ')}`);
 }
+
